fa4bd8ec0038c1bfd8cec6ac33df7fd91a9498db
Aaron Lin <Aaronl36033@gmail.com>
2024-06-09 19:25:19 +1000

    initial setup

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello world")
+import pgzrun
+
+WIDTH = 800
+HEIGHT = 600
+
+sprites = []
+buttons = []
+#Add your code below
+
+def draw():
+    screen.clear()
+    for s in sprites:
+        s.draw()
+    for b in buttons:
+        b.draw()
+    # elements need to be draw here
+
+def update():
+    # any global variable need to be updated need specify global
+    pass
+
+def on_mouse_down(pos):
+    for b in buttons:
+        b.on_mouse_down(pos)
+
+def on_mouse_up(pos):
+    for b in buttons:
+        b.on_mouse_up(pos)
+
+pgzrun.go() # Must be last line
