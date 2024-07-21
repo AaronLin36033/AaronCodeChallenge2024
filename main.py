5b8ee4a6b63c14c1edf24687e0624ddcaf6e4463
Aaron Lin <Aaronl36033@gmail.com>
2024-07-21 20:04:44 +1000

    Abby

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ sprites = []
 buttons = []
 current_step = 0
 
-
 background = Actor("classroom", (400, 300))
 background.scale = 0.6
 
@@ -27,21 +26,75 @@ def step_changed():
         devin.show = False
         abby.show = False
         start_button.show = True 
-    if current_step == 1:
+        next_button.show = False
+        problem_button.show = False
+        solution_button.show = False
+    elif current_step == 1:
         devin.show = True
         abby.show = True
         start_button.show = False 
-        devin.message = "Hello, I am Devin. I am going to show you what a school in the future looks like. "
+        next_button.show = True
+        problem_button.show = False
+        solution_button.show = False
+        devin.message = "Hello, I am Devin. I am going to show you what a school in the future looks like."
+    elif current_step == 2:
+        devin.show = True
+        abby.show = True
+        next_button.show = False
+        problem_button.show = True
+        solution_button.show = False
+        abby.message = "Have you noticed how much electricity the lights in this classroom use?"
+    elif current_step == 3:
+        devin.show = True
+        abby.show = True
+        next_button.show = False
+        problem_button.show = False
+        solution_button.show = True
+        devin.message = "It's not great for the environment. But there are solutions!"
+    elif current_step == 4:
+        devin.show = True
+        abby.show = True
+        next_button.show = False
+        problem_button.show = False
+        solution_button.show = False
+        abby.message = "Let's use natural light fiber and solar panels to reduce electricity usage."
 
 def start_button_action():
     global current_step
     current_step = 1
     step_changed()
 
+def next_button_action():
+    global current_step
+    current_step += 1
+    step_changed()
+
+def problem_button_action():
+    global current_step
+    current_step = 3
+    step_changed()
+
+def solution_button_action():
+    global current_step
+    current_step = 4
+    step_changed()
+
 start_button = Button("start", (WIDTH // 2, HEIGHT // 2), start_button_action)
 start_button.scale = 0.5
 buttons.append(start_button)
- 
+
+next_button = Button("next", (WIDTH // 2 + 100, HEIGHT // 2 + 200), next_button_action)
+next_button.scale = 0.5
+buttons.append(next_button)
+
+problem_button = Button("problem", (WIDTH // 2 - 200, HEIGHT // 2 + 200), problem_button_action)
+problem_button.scale = 0.5
+buttons.append(problem_button)
+
+solution_button = Button("solution", (WIDTH // 2 + 200, HEIGHT // 2 + 200), solution_button_action)
+solution_button.scale = 0.5
+buttons.append(solution_button)
+
 def draw():
     screen.clear()
     background.draw()
@@ -62,4 +115,4 @@ def on_mouse_up(pos):
         b.on_mouse_up(pos)
 
 step_changed()
-pgzrun.go()  
+pgzrun.go()
