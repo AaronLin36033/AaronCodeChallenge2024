a0eb10445f30d7a1ab9f7913e621fe88d917c869
Capsen Lu <capsen_lu@hotmail.com>
2024-08-15 20:06:53 +1000

    add main change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 import pgzrun
 from pgzhelper import *
-from button import Button  
+from button import Button
 from sprite import Sprite
 from pgzero import clock, music
 
@@ -16,11 +17,11 @@ clicked_classroom_buttons = set()
 background = Actor("classroom", (400, 300))
 
 devin = Sprite("devin", (700, 450))
-devin.scale = 0.5 
+devin.scale = 0.5
 sprites.append(devin)
 
 abby = Sprite("abby", (100, 450))
-abby.scale = 0.5 
+abby.scale = 0.5
 sprites.append(abby)
 
 light = Sprite("lighting", (400, 450))
@@ -28,24 +29,28 @@ light.scale = 0.5
 light.show = False
 sprites.append(light)
 
+
 def step_changed():
+    story_button.show = False
+    info_button.show = False
+    next_button.show = False
+    problem_button.show = False
+    solution_button.show = False
+    roofbutton.show = False
+    vrbutton.show = False
+    computerbutton.show = False
+    whiteboardbutton.show = False
     if music.is_playing:
         music.stop()
     if current_step == 0:
         devin.show = False
         abby.show = False
-        start_button.show = True 
-        next_button.show = False
-        problem_button.show = False
-        solution_button.show = False
-        roofbutton.show = False
-        vrbutton.show = False
-        computerbutton.show = False
-        whiteboardbutton.show = False
+        start_button.show = True
     elif current_step == 1:
+        background.image="classroom"
         devin.show = True
         abby.show = True
-        start_button.show = False 
+        start_button.show = False
         next_button.show = True
         problem_button.show = False
         solution_button.show = False
@@ -57,7 +62,7 @@ def step_changed():
         next_button.show = True
         abby.message = "That's great, let's see what is different between now and the future in the classroom."
         music.play_once("seethefuture")
-        devin.message=""
+        devin.message = ""
     elif current_step == 3:
         devin.show = True
         abby.show = True
@@ -87,23 +92,23 @@ def step_changed():
     elif current_step == 11:
         abby.message = "Have you noticed how much electricity the lights in this classroom use?"
         music.play_once("howmuchelectricity")
-        devin.message=""
+        devin.message = ""
         clock.schedule(next_button_action, 5)
     elif current_step == 12:
-        devin.message="It's not great for the environment. But there are solutions!"
+        devin.message = "It's not great for the environment. But there are solutions!"
         music.play_once("notgreatforenviroment")
-        abby.message=""
+        abby.message = ""
         clock.schedule(next_button_action, 4)
     elif current_step == 13:
-        abby.message="What do you think?"
+        abby.message = "What do you think?"
         music.play_once("whatdoyouthink")
         clock.schedule(next_button_action, 2)
-        devin.message=""
+        devin.message = ""
     elif current_step == 14:
-        abby.message="Let's use natural light fiber and solar panels to reduce electricity usage."
+        abby.message = "Let's use natural light fiber and solar panels to reduce electricity usage."
         music.play_once("naturallight")
-        devin.message=""
-        light.show=True
+        devin.message = ""
+        light.show = True
         # TODO: Show the light fiber picture
         clock.schedule(goto_classroom_buttons, 6)
     elif current_step == 21:
@@ -114,7 +119,7 @@ def step_changed():
     elif current_step == 22:
         devin.message = ""
         abby.message = "VR headsets allow virtual field trips, making immersive experiences accessible to all students."
-        music.play_once("VRheadset")
+        music.play_once("vrheadset")
         clock.schedule(goto_classroom_buttons, 7)
     elif current_step == 23:
         devin.message = ""
@@ -152,30 +157,53 @@ def step_changed():
         whiteboardbutton.show = False
         devin.message = ""
         abby.message = "That's amazing, I can't wait for the future for those technologies."
+        music.play_once("waitfuture")
         clock.schedule(next_button_action, 5)
     elif current_step == 51:
-        devin.message = "Actually these technology are available in these days."
+        devin.message = "Actually, these technologies are available these days."
         abby.message = ""
+        music.play_once("availablethesedays")
+        clock.schedule(next_button_action, 4)
+    elif current_step == 52:
+        devin.message = "Please return to the Menu screen and check out more from the information button."
+        music.play_once("returnmenu")
+        clock.schedule(goto_Menu_buttons, 6)
+    elif current_step == 60:
+        background.image = "menu"
+        abby.message = ""
+        devin.message = ""
+        info_button.show = True
+        story_button.show = True
+    elif current_step == 70:
+        background.image = "" # more info page
+        abby.message = ""
+        devin.message = ""
+
 
 def start_button_action():
     global current_step
-    current_step = 1
+    current_step = 60
     step_changed()
+    start_button.show = False
+
 
 def next_button_action():
     global current_step
     current_step += 1
     step_changed()
 
+
 def problem_button_action():
     global current_step
     current_step = 3
     step_changed()
 
+
 def solution_button_action():
     global current_step
     current_step = 4
     step_changed()
+
 
 def roofbutton_action():
     global current_step
@@ -183,11 +211,13 @@ def roofbutton_action():
     step_changed()
     clicked_classroom_buttons.add("roof")
 
+
 def vrbutton_action():
     global current_step
     current_step = 21
     step_changed()
     clicked_classroom_buttons.add("vr")
+
 
 def computerbutton_action():
     global current_step
@@ -195,11 +225,13 @@ def computerbutton_action():
     step_changed()
     clicked_classroom_buttons.add("computer")
 
+
 def whiteboardbutton_action():
     global current_step
     current_step = 41
     step_changed()
     clicked_classroom_buttons.add("whiteboard")
+
 
 def check_classroom_buttons():
     global current_step
@@ -207,9 +239,26 @@ def check_classroom_buttons():
         current_step = 50
         step_changed()
 
+
 def goto_classroom_buttons():
     global current_step
     current_step = 10
+    step_changed()
+
+
+def goto_Menu_buttons():
+    global current_step
+    current_step = 60
+    step_changed()
+
+def goto_story_action():
+    global current_step
+    current_step = 1
+    step_changed()
+
+def goto_info_action():
+    global current_step
+    current_step = 70
     step_changed()
 
 
@@ -218,17 +267,28 @@ start_button = Button("start", (WIDTH // 2, HEIGHT // 2), start_button_action)
 start_button.scale = 0.5
 buttons.append(start_button)
 
-next_button = Button("next", (WIDTH // 2 + 100, HEIGHT // 2 + 200), next_button_action)
+next_button = Button("next", (WIDTH // 2 + 100, HEIGHT //
+                     2 + 200), next_button_action)
 next_button.scale = 0.5
 buttons.append(next_button)
 
-problem_button = Button("problem", (WIDTH // 2 - 200, HEIGHT // 2 + 200), problem_button_action)
+problem_button = Button(
+    "problem", (WIDTH // 2 - 200, HEIGHT // 2 + 200), problem_button_action)
 problem_button.scale = 0.2
 buttons.append(problem_button)
 
-solution_button = Button("solution", (WIDTH // 2 + 200, HEIGHT // 2 + 200), solution_button_action)
+solution_button = Button(
+    "solution", (WIDTH // 2 + 200, HEIGHT // 2 + 200), solution_button_action)
 solution_button.scale = 0.2
 buttons.append(solution_button)
+
+story_button = Button("storybutton", pos=(400, 200), callback=goto_story_action, show=False)
+story_button.scale=0.3
+buttons.append(story_button)
+
+info_button = Button("information", pos=(400, 400), callback=goto_info_action,  show=False)
+info_button.scale=0.3
+buttons.append(info_button)
 
 # Hover buttons setup
 roofbutton = Actor("roof", (400, 80))
@@ -251,20 +311,25 @@ whiteboardbutton.imagename = "whiteboard"
 whiteboardbutton.action = whiteboardbutton_action
 hover_buttons.append(whiteboardbutton)
 
+
 def draw():
     screen.clear()
     background.draw()
     for h in hover_buttons:
-        h.draw()
+        if h.show :
+            h.draw()
     for s in sprites:
         s.draw()
     for b in buttons:
         b.draw()
-    #draw step on screen
-    screen.draw.text("step: "+str(current_step), (680,10), color=(0, 0, 0), fontsize=32)
+    # draw step on screen
+    screen.draw.text("step: "+str(current_step), (680, 10),
+                     color=(0, 0, 0), fontsize=32)
+
 
 def update():
     pass
+
 
 def on_mouse_down(pos):
     for b in buttons:
@@ -274,9 +339,11 @@ def on_mouse_down(pos):
             if h.get_rect().collidepoint(pos):
                 h.action()
 
+
 def on_mouse_up(pos):
     for b in buttons:
         b.on_mouse_up(pos)
+
 
 def on_mouse_move(pos):
     for h in hover_buttons:
@@ -286,5 +353,7 @@ def on_mouse_move(pos):
             else:
                 h.image = h.imagename
 
+
 step_changed()
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 pgzrun.go()
